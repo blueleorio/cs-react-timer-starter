f3e5208ee12e40193a8d524e8877fdf1a22ab614
BeluKotu <vietthanh456@gmail.com>
2023-12-13 12:02:45 +0700

    lookupSolution

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,11 +1,13 @@
 import { useState, useRef } from "react";
 
+// TODO:  Reference: https://www.w3schools.com/js/js_timing.asp
+
 const useTimer = (ini = 0) => {
   const [time, setTime] = useState(0); // TODO: code here
 
   const isStart = null; // TODO: code here
   const active = null; // TODO: code here
-  const refInterval = ""; // TODO: code here
+  const refInterval = 0; // TODO: code here
 
   const startTimer = () => {
     // TODO: code here;
