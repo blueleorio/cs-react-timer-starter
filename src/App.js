1cc0b4d46bb0691ab0684ee659e1c9cf6068d47a
BeluKotu <vietthanh456@gmail.com>
2023-12-21 06:33:53 +0700

    Final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from "react";
 import useTimer from "./useTimer";
 import { formatTime } from "./formatTime";
+import { useGlitch } from "react-powerglitch";
 
 function App() {
-  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
-
+  const { time, startTimer, stopTimer, resetTimer, active, text, start } =
+    useTimer(0);
+  const glitch = useGlitch({
+    playMode: "always",
+    createContainers: true,
+    hideOverflow: false,
+    timing: {
+      duration: 1000,
+    },
+    glitchTimeSpan: {
+      start: 0.0,
+      end: 0.9,
+    },
+    shake: {
+      velocity: 15,
+      amplitudeX: 0.2,
+      amplitudeY: 0.2,
+    },
+    slice: {
+      count: 6,
+      velocity: 15,
+      minHeight: 0.02,
+      maxHeight: 0.15,
+      hueRotate: true,
+    },
+    pulse: false,
+  });
   return (
     <div className="App container">
-      <h1>Time Machine</h1>
+      <h1>{text}</h1>
       <div className="timer__wrapper">
         <div className="timer__display">
-          <p>{formatTime(time)}</p>
+          <p>
+            {" "}
+            <span ref={glitch.ref}>{formatTime(time)}</span>
+          </p>
         </div>
         <div className="button__wrapper">
-          <button className="button" onClick={stopTimer}>
-            Stop
+          <button
+            className="button"
+            onClick={() => {
+              stopTimer();
+              glitch.stopGlitch();
+            }}
+          >
+            STOP
           </button>
-          <button className="button" ref={active} onClick={startTimer}>
-            Start
+          <button
+            className="button"
+            ref={active}
+            onClick={() => {
+              startTimer();
+              glitch.startGlitch();
+            }}
+          >
+            START
           </button>
-          <button className="button" onClick={resetTimer}>
-            Reset
+          <button
+            className="button"
+            onClick={() => {
+              resetTimer();
+              glitch.stopGlitch();
+            }}
+          >
+            RESET
           </button>
         </div>
       </div>
--- src/useTimer.js
+++ src/useTimer.js
@@ -7,9 +7,40 @@ const useTimer = (ini = 0) => {
   const isStart = useRef(true);
   const active = useRef();
   const refInterval = useRef(0);
+  const [text, setText] = useState("Time Machine");
+
+  const startAudioRef = useRef(new Audio("/gud.mp3"));
+  const stopAudioRef = useRef(new Audio("/dio.mp3"));
+  const restAudioRef = useRef(new Audio("/mac.mp3"));
+
+  const playAudio = (audioRef, loop, volume = 1) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+    } else {
+      // No need to recreate the Audio element if it already exists
+      audioRef.current = new Audio(audioRef.current.src);
+    }
+    audioRef.current.volume = volume;
+    if (loop) {
+      audioRef.current.loop = true;
+    }
+
+    audioRef.current.play();
+  };
+
+  const start = () => {
+    playAudio(startAudioRef, true);
+  };
+
+  const stop = () => {
+    playAudio(stopAudioRef, false, 0.2);
+  };
+
+  const reset = () => {
+    playAudio(restAudioRef, false);
+  };
 
   const startTimer = () => {
-    // TODO: code here;
     active.current.disabled = true;
     isStart.current = true;
     refInterval.current = setInterval(() => {
@@ -17,20 +48,35 @@ const useTimer = (ini = 0) => {
         setTime((time) => time + 1);
       }
     }, 1000);
+    setText("Time goes brrrrrr...!!!");
+    start();
   };
+
   const stopTimer = () => {
-    // TODO: code here;
     isStart.current = false;
     clearInterval(refInterval.current);
     active.current.disabled = false;
+    setText("Za Warudo");
+    if (startAudioRef.current) {
+      startAudioRef.current.pause();
+      startAudioRef.current.currentTime = 0;
+    }
+    stop();
   };
+
   const resetTimer = () => {
-    // TODO: code here;
     setTime(0);
     clearInterval(refInterval.current);
     active.current.disabled = false;
+    setText("T i m e m i T");
+    if (startAudioRef.current) {
+      startAudioRef.current.pause();
+      startAudioRef.current.currentTime = 0;
+    }
+    reset();
   };
 
-  return { time, startTimer, stopTimer, resetTimer, active };
+  return { time, startTimer, stopTimer, resetTimer, active, text, start, stop };
 };
+
 export default useTimer;
